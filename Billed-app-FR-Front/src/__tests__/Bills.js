afc995daddaa4e3b78114ff60fa8a5fd7ebd23ef
Kévin Fischer <kevinfischer@live.fr>
2023-11-16 16:00:47 +0100

    :white_check_mark: 76% coverage on Bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,13 +2,14 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from '@testing-library/dom';
+import { screen, waitFor, fireEvent, wait } from '@testing-library/dom';
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
 import { ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
-
 import router from '../app/Router.js';
+import Bills from '../containers/Bills.js';
+import mockedBills from '../__mocks__/store.js';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
@@ -29,6 +30,7 @@ describe('Given I am connected as an employee', () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
+      // check if the icon is highlighted by checking if it has the class 'active-icon'
       expect(windowIcon.classList.contains('active-icon')).toBe(true);
     });
 
@@ -43,5 +45,58 @@ describe('Given I am connected as an employee', () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+  });
+});
+
+// Test suite for interactions on the Bills Page
+describe('Given I am connected as an employee', () => {
+  // Test to verify if the 'New Bill' button has a click event listener
+  test('Button new-bill should have an event listener for click', () => {
+    // Mock the navigation function
+    const onNavigate = jest.fn();
+
+    // Instantiate the Bills class with necessary mocks
+    const myBillsInstance = new Bills({
+      document,
+      onNavigate,
+      store: mockedBills,
+      localStorage: window.localStorage,
+    });
+    myBillsInstance.handleClickNewBill = jest.fn();
+
+    // Retrieve the 'New Bill' button and simulate a click
+    const buttonNewBill = document.querySelector(
+      `button[data-testid="btn-new-bill"]`
+    );
+    expect(buttonNewBill).toBeDefined();
+    buttonNewBill.click();
+
+    // Verify if the navigation function was called with the correct path
+    expect(myBillsInstance.onNavigate).toHaveBeenCalledWith(
+      ROUTES_PATH['NewBill']
+    );
+  });
+
+  // Test to verify if each 'eye' icon has a click event listener
+  test('Each icon-eye should have an event listener for click', () => {
+    // Mock jQuery's modal function
+    jQuery.fn.modal = () => {};
+
+    // Retrieve all 'eye' icons
+    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
+    expect(iconEye).toBeDefined();
+
+    // Instantiate the Bills class with necessary mocks
+    const myBillsInstance = new Bills({
+      document,
+      onNavigate,
+      store: mockedBills,
+      localStorage,
+    });
+    myBillsInstance.handleClickIconEye = jest.fn();
+
+    // Simulate a click on the first 'eye' icon and verify if the event listener was called
+    iconEye[0].click();
+    expect(myBillsInstance.handleClickIconEye).toHaveBeenCalled();
   });
 });
